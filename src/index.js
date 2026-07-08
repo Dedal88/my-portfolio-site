ef76014d62b9a920fb376c0be93cebf9336cd64d
Dedal88 <audit37msv@yandex.ru>
2026-07-08 19:13:46 +0300

    Updates

---
+++ src/index.js
@@ -0,0 +1,91 @@
+import {
+  createProject,
+  animationAppearanceElements,
+  createElement,
+} from '../src/modules/Utilities';
+
+import { headerScroll } from './modules/Header';
+import { sidebarMenu } from './modules/sidebar-menu';
+import { dataProjects } from './modules/Data';
+import { connectModalWindow } from './modules/Modal';
+
+headerScroll();
+sidebarMenu();
+connectModalWindow();
+
+const projectsContainer = document.querySelector('.projects__wrapper');
+
+animationAppearanceElements(projectsContainer, dataProjects, createProject);
+
+const sliderImages = document.querySelectorAll('.slider__img');
+const sliderLine = document.querySelector('.slider__line');
+const sliderDots = document.querySelectorAll('.slider__dot');
+const sliderBtnNext = document.querySelector('.slider__btn-next');
+const sliderBtnPrev = document.querySelector('.slider__btn-prev');
+
+let sliderCount = 0;
+let sliderWidth;
+
+window.addEventListener('resize', showSlide);
+
+sliderBtnNext.addEventListener('click', nextSlide);
+sliderBtnPrev.addEventListener('click', prevSlide);
+
+function showSlide() {
+  sliderWidth = document.querySelector('.slider').offsetWidth;
+  sliderLine.style.width = sliderWidth * sliderImages.length + 'px';
+  sliderImages.forEach((item) => (item.style.width = sliderWidth + 'px'));
+  rollSlider();
+}
+
+function initSlider() {
+  Promise.all(
+    Array.from(sliderImages).map((img) => {
+      if (img.complete) return Promise.resolve();
+      return new Promise((resolve) => {
+        img.onload = resolve;
+        img.onerror = resolve;
+      });
+    }),
+  ).then(() => {
+    showSlide();
+  });
+}
+
+initSlider();
+
+function nextSlide() {
+  sliderCount++;
+  if (sliderCount >= sliderImages.length) {
+    sliderCount = 0;
+  }
+
+  rollSlider();
+  thisSlide(sliderCount);
+}
+
+function prevSlide() {
+  sliderCount--;
+  if (sliderCount < 0) {
+    sliderCount = sliderImages.length - 1;
+  }
+  rollSlider();
+  thisSlide(sliderCount);
+}
+
+function rollSlider() {
+  sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
+}
+
+function thisSlide(index) {
+  sliderDots.forEach((item) => item.classList.remove('active-dot'));
+  sliderDots[index].classList.add('active-dot');
+}
+
+sliderDots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    sliderCount = index;
+    rollSlider();
+    thisSlide(sliderCount);
+  });
+});
---
+++ src/modules/Modal.js
@@ -0,0 +1,121 @@
+import { createElement } from './Utilities';
+
+class modalWindow {
+  static createModalWindow(textInWin = undefined, elementInWin = undefined) {
+    const modalWrap = createElement({
+      tag: 'div',
+      classes: ['modalWrap'],
+    });
+
+    const modalWindow = createElement({
+      tag: 'div',
+      text: `${textInWin}`,
+      classes: ['modalWindow'],
+    });
+    modalWrap.append(modalWindow);
+
+    if (elementInWin) {
+      modalWindow.append(elementInWin);
+    }
+
+    const modalCloseButton = createElement({
+      tag: 'button',
+      text: 'Закрыть',
+      classes: ['modalCloseButton'],
+    });
+    modalWindow.append(modalCloseButton);
+    modalCloseButton.addEventListener('click', function () {
+      modalWrap.classList.remove('open');
+    });
+
+    modalWrap.addEventListener('click', function (event) {
+      if (event.target === modalWrap) {
+        modalWrap.classList.remove('open');
+      }
+    });
+
+    const handleEscapeKey = function (event) {
+      if (event.key === 'Escape' && modalWrap.classList.contains('open')) {
+        modalWrap.classList.remove('open');
+      }
+    };
+    document.addEventListener('keydown', handleEscapeKey);
+
+    return modalWrap;
+  }
+
+  static openModalWindow(modalWinElem) {
+    modalWinElem.classList.add('open');
+  }
+}
+
+function createModalContactsContent() {
+  const contentForModal = createElement({
+    classes: ['modalContentContainer'],
+  });
+
+  const emailLink = createElement({
+    tag: 'a',
+    classes: ['social-icon'],
+  });
+  emailLink.setAttribute('href', 'mailto:audit37msv@yandex.ru');
+
+  const emailIcon = createElement({
+    tag: 'img',
+    classes: ['social-icon__image'],
+  });
+  emailIcon.setAttribute('src', '../../../src/img/icons/mail.svg');
+  emailIcon.setAttribute('alt', 'Email icon');
+
+  const emailText = document.createTextNode(
+    'Эл. почта:\u00A0\u00A0 audit37msv@yandex.ru',
+  );
+  emailLink.append(emailIcon, emailText);
+
+  const telegramLink = createElement({
+    tag: 'a',
+    classes: ['social-icon'],
+  });
+  telegramLink.setAttribute('href', 'tg://resolve?domain=Sergey');
+
+  const telegramIcon = createElement({
+    tag: 'img',
+    classes: ['social-icon__image'],
+  });
+  telegramIcon.setAttribute('src', '../../../src/img/icons/telegram.svg');
+  telegramIcon.setAttribute('alt', 'Telegram icon');
+
+  const telegramText = document.createTextNode('Tелеграм:\u00A0\u00A0 Sergey');
+  telegramLink.append(telegramIcon, telegramText);
+
+  const phoneLink = createElement({
+    tag: 'a',
+    classes: ['social-icon'],
+  });
+  phoneLink.setAttribute('href', 'tel:+79158326698');
+
+  const phoneIcon = createElement({
+    tag: 'img',
+    classes: ['social-icon__image'],
+  });
+  phoneIcon.setAttribute('src', '../../../src/img/icons/phone.svg');
+  phoneIcon.setAttribute('alt', 'Phone icon');
+
+  const phoneText = document.createTextNode(
+    'Телефон:\u00A0\u00A0 +79158326698\u00A0\u00A0(Сергей)',
+  );
+  phoneLink.append(phoneIcon, phoneText);
+
+  contentForModal.append(emailLink, telegramLink, phoneLink);
+  return contentForModal;
+}
+
+export function connectModalWindow() {
+  const modalContactsContent = createModalContactsContent();
+  const modal = modalWindow.createModalWindow('', modalContactsContent);
+  document.body.appendChild(modal);
+  const showContactsButton = document.querySelector('.header__button');
+  showContactsButton.addEventListener('click', () => {
+    modalWindow.openModalWindow(modal);
+  });
+}
